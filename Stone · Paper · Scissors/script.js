5ed97757cd6c65d9eef34edef3d4dc8422b0b5b5
itzz-sojwal <sojwalnaikwadi2023@gmail.com>
2026-07-29 15:10:54 +0530

    Done with html and css

---
+++ Stone · Paper · Scissors/script.js
@@ -0,0 +1,9 @@
+const choices = ["Stone","Paper","Scissor"]
+
+function generateRobotChoice() {
+  const choices = ["Stone","Paper","Scissor"]
+  const i = Math.random
+}
+
+const i = Math.random()
+console.log(Math.random())
